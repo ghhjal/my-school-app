cbf1f53ee86d68e0b8c9cca4355f6f7b666431d1
ghhjal <ghhjal@gmail.com>
2026-01-06 20:34:02 +0300

    Update app.py

--- app.py
+++ app.py
@@ -330,8 +330,8 @@ if st.session_state.role == "teacher":
                         ])
                         st.success("✅ تم النشر بنجاح وفق خياراتك.")
                         st.cache_data.clear(); st.rerun()
-                        except:
-                            st.error("⚠️ حدث خطأ أثناء الاتصال بقاعدة البيانات.")
+                            except:
+                                st.error("⚠️ حدث خطأ أثناء الاتصال بقاعدة البيانات.")
                     else:
                         st.warning("⚠️ يرجى كتابة عنوان للتنبيه أولاً.")
         
